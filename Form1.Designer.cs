4801512a5d750a12b285ab27717c8589b10bbc35
pims-diary <speak2priyankaghosh@gmail.com>
2024-07-11 23:02:35 +1200

    Basic project uploaded

---
+++ Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace parcelCompany
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.staffbttn = new System.Windows.Forms.Button();
+            this.customerbttn = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Uighur", 26F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(294, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(564, 62);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "WELCOME TO UML LOGISTICS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Mistral", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(392, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(326, 38);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Your one stop delivery partner";
+            // 
+            // staffbttn
+            // 
+            this.staffbttn.BackColor = System.Drawing.Color.White;
+            this.staffbttn.Font = new System.Drawing.Font("Microsoft Uighur", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.staffbttn.ForeColor = System.Drawing.Color.Red;
+            this.staffbttn.Location = new System.Drawing.Point(80, 257);
+            this.staffbttn.Name = "staffbttn";
+            this.staffbttn.Size = new System.Drawing.Size(289, 78);
+            this.staffbttn.TabIndex = 4;
+            this.staffbttn.Text = "Staff";
+            this.staffbttn.UseVisualStyleBackColor = false;
+            this.staffbttn.Click += new System.EventHandler(this.staffbttn_Click);
+            // 
+            // customerbttn
+            // 
+            this.customerbttn.Font = new System.Drawing.Font("Microsoft Uighur", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.customerbttn.ForeColor = System.Drawing.Color.Red;
+            this.customerbttn.Location = new System.Drawing.Point(80, 417);
+            this.customerbttn.Name = "customerbttn";
+            this.customerbttn.Size = new System.Drawing.Size(289, 76);
+            this.customerbttn.TabIndex = 5;
+            this.customerbttn.Text = "Customer";
+            this.customerbttn.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::parcelCompany.Properties.Resources.pngtree_courier_with_a_lot_of_parcels_isolated_on_white_background_illustration_png_image_13122701;
+            this.pictureBox2.Location = new System.Drawing.Point(741, 185);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(483, 401);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::parcelCompany.Properties.Resources.UML__1_;
+            this.pictureBox1.Location = new System.Drawing.Point(88, -8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(215, 172);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1236, 674);
+            this.Controls.Add(this.customerbttn);
+            this.Controls.Add(this.staffbttn);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button staffbttn;
+        private System.Windows.Forms.Button customerbttn;
+    }
+}
+
---
+++ loginPageStaff.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace parcelCompany
+{
+    partial class loginPageStaff
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.log = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.username = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.loginbttn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // log
+            // 
+            this.log.AutoSize = true;
+            this.log.Font = new System.Drawing.Font("Microsoft Uighur", 26F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.log.Location = new System.Drawing.Point(448, 59);
+            this.log.Name = "log";
+            this.log.Size = new System.Drawing.Size(144, 62);
+            this.log.TabIndex = 0;
+            this.log.Text = "LOGIN";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Uighur", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(176, 198);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 43);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Uighur", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(184, 327);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 43);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password:";
+            // 
+            // username
+            // 
+            this.username.Location = new System.Drawing.Point(415, 198);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(322, 26);
+            this.username.TabIndex = 3;
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(415, 327);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(322, 26);
+            this.password.TabIndex = 4;
+            // 
+            // loginbttn
+            // 
+            this.loginbttn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.loginbttn.Font = new System.Drawing.Font("Microsoft Uighur", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginbttn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.loginbttn.Location = new System.Drawing.Point(459, 472);
+            this.loginbttn.Name = "loginbttn";
+            this.loginbttn.Size = new System.Drawing.Size(111, 61);
+            this.loginbttn.TabIndex = 5;
+            this.loginbttn.Text = "Login";
+            this.loginbttn.UseVisualStyleBackColor = false;
+            this.loginbttn.Click += new System.EventHandler(this.loginbttn_Click);
+            // 
+            // loginPageStaff
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(1017, 623);
+            this.Controls.Add(this.loginbttn);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.log);
+            this.Name = "loginPageStaff";
+            this.Text = "loginPageStaff";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label log;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Button loginbttn;
+    }
+}
